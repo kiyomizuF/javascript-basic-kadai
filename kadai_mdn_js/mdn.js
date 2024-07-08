4a4f9a10d7e033072f5241caf02f994be1b0c0db
kiyomizuF <shimizu.itsuki@gmail.com>
2024-07-08 19:57:43 +0900

    日付の取得方法をMDNで調べる課題を修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,8 @@
 const today = new Date();
-const month12 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 console.log(
   today.getFullYear() +
     "年" +
-    month12[today.getMonth()] +
+    Number(today.getMonth() + 1) +
     "月" +
     today.getDate() +
     "日"
