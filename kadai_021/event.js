f770fc7942c529557c513ae8e9506f117e666f21
kiyomizuF <shimizu.itsuki@gmail.com>
2024-07-13 10:43:19 +0900

    非同期処理の課題を作成

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const changeBtn = document.getElementById("btn");
+
+changeBtn.addEventListener("click", () => {
+  const changeText = document.getElementById("text");
+  setTimeout(() => {
+    changeText.textContent = "ボタンをクリックしました";
+  }, 2000);
+});
